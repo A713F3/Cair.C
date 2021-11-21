c16a253aa86fc297a480ad9260189194465c7a98
Ali Efe <aliefekaragul@gmail.com>
2021-11-21 18:39:35 +0300

    changed rendering method

--- cair.c
+++ cair.c
@@ -15,38 +15,41 @@ typedef struct log{
     char c;
 } LOG;
 
-
 // Set cars position (0:Left-Lane 1:Right-Line)
 void CARsetPOS(CAR * car, int pos);
 
 // Initialize grid with character c 
-void initGrid(char * grid, char c);
+void clearGrid(char * grid, char c);
 
 // Render grid on console
-void renderGrid(char * grid, CAR car);
+void renderGrid(char * grid);
 
+// Update grid
+void updateGrid(char * grid, CAR car, LOG log);
 
 // Left and right lane coordinates ([0]:Left [1]:Right)
 const int car_pos_cords[2][2] = {
-    {2, HEIGHT - 2},
+    {1, HEIGHT - 2},
     {4, HEIGHT - 2}
 };
 
 const int log_init_cords[2][2] = {
-    {2, 1},
+    {1, 1},
     {4, 1}
 };
 
 int main(){
     char cGrid[HEIGHT * WIDTH]; //Background grid
-    initGrid(cGrid, '#');
+    clearGrid(cGrid, '#');
 
     CAR car = {.x = car_pos_cords[0][0], .y = car_pos_cords[0][1], .c = ' '};
     LOG log = {.x = log_init_cords[0][0], .y = log_init_cords[0][1], .c = 'L'};
 
-    CARsetPOS(&car, 1);
+    CARsetPOS(&car, 0);
 
-    renderGrid(cGrid, car);
+    updateGrid(cGrid, car, log);
+
+    renderGrid(cGrid);
 
     return 0;
 }
@@ -56,7 +59,7 @@ void CARsetPOS(CAR * car, int pos){
     car -> y = car_pos_cords[pos][1];
 }
 
-void initGrid(char * grid, char c){
+void clearGrid(char * grid, char c){
     int y, x;
     for (y = 0; y < HEIGHT; y++){
         for (x = 0; x < WIDTH; x++){
@@ -65,14 +68,19 @@ void initGrid(char * grid, char c){
     }
 }
 
-void renderGrid(char * grid, CAR car){
+void renderGrid(char * grid){
     int y, x;
     for (y = 0; y < HEIGHT; y++){
         for (x = 0; x < WIDTH; x++){
-            if (x == WIDTH / 2)           printf("|");
-            if (x == car.x && y == car.y) printf("%c", car.c);
-            else                          printf("%c", grid[WIDTH * y + x]);
+            if (x == WIDTH / 2) printf("|");
+            printf("%c", grid[WIDTH * y + x]);
         }
         printf("\n");
     }
+}
+
+void updateGrid(char * grid, CAR car, LOG log){
+    clearGrid(grid, '#');
+    grid[WIDTH * car.y + car.x] = car.c;
+    grid[WIDTH * log.y + log.x] = log.c;
 }
