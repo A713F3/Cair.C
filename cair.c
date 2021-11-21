6a638172585860bf89d75458650bc6cf3a8a9f80
Ali Efe <aliefekaragul@gmail.com>
2021-11-21 18:54:50 +0300

    added object struct and changed update function

--- cair.c
+++ cair.c
@@ -4,36 +4,33 @@
 #define HEIGHT 8
 #define WIDTH  6
 
-typedef struct car{
-    int x, y;
-    char c;
-} CAR;
-
-typedef struct log{
-    int x, y;
-    int v_x, v_y;
-    char c;
-} LOG;
+typedef struct obj{
+    int x, y;     //Position
+    int v_x, v_y; //Velocity
+    char c;       //Character
+} OBJECT;
 
 // Set cars position (0:Left-Lane 1:Right-Line)
-void CARsetPOS(CAR * car, int pos);
+void setPOS(OBJECT * obj, int x, int y);
 
 // Initialize grid with character c 
 void clearGrid(char * grid, char c);
 
+// Update grid
+void updateGrid(char * grid, OBJECT * objs);
+
 // Render grid on console
 void renderGrid(char * grid);
 
-// Update grid
-void updateGrid(char * grid, CAR car, LOG log);
 
 // Left and right lane coordinates ([0]:Left [1]:Right)
-const int car_pos_cords[2][2] = {
+const int car_cords[2][2] = {
     {1, HEIGHT - 2},
     {4, HEIGHT - 2}
 };
 
-const int log_init_cords[2][2] = {
+// Log starting coordinates ([0]:Left [1]:Right)
+const int log_cords[2][2] = {
     {1, 1},
     {4, 1}
 };
@@ -42,21 +39,21 @@ int main(){
     char cGrid[HEIGHT * WIDTH]; //Background grid
     clearGrid(cGrid, '#');
 
-    CAR car = {.x = car_pos_cords[0][0], .y = car_pos_cords[0][1], .c = ' '};
-    LOG log = {.x = log_init_cords[0][0], .y = log_init_cords[0][1], .c = 'L'};
+    OBJECT car = {.x = car_cords[0][0], .y = car_cords[0][1], .c = ' '};
+    OBJECT log = {.x = log_cords[0][0], .y = log_cords[0][1], .c = 'L'};
 
-    CARsetPOS(&car, 0);
+    OBJECT objs[2] = {car, log};
 
-    updateGrid(cGrid, car, log);
+    updateGrid(cGrid, objs);
 
     renderGrid(cGrid);
 
     return 0;
 }
 
-void CARsetPOS(CAR * car, int pos){
-    car -> x = car_pos_cords[pos][0]; 
-    car -> y = car_pos_cords[pos][1];
+void setPOS(OBJECT * obj, int x, int y){
+    obj -> x = x; 
+    obj -> y = y;
 }
 
 void clearGrid(char * grid, char c){
@@ -65,6 +62,16 @@ void clearGrid(char * grid, char c){
         for (x = 0; x < WIDTH; x++){
             grid[WIDTH * y + x] = c;
         }
+    }
+}
+
+void updateGrid(char * grid, OBJECT * objs){
+    clearGrid(grid, '#');
+
+    int i, size = (sizeof(grid)/sizeof(*grid));
+
+    for (i = 0; i < size; i++){
+        grid[WIDTH * objs[i].y + objs[i].x] = objs[i].c;
     }
 }
 
@@ -77,10 +84,4 @@ void renderGrid(char * grid){
         }
         printf("\n");
     }
-}
-
-void updateGrid(char * grid, CAR car, LOG log){
-    clearGrid(grid, '#');
-    grid[WIDTH * car.y + car.x] = car.c;
-    grid[WIDTH * log.y + log.x] = log.c;
 }
